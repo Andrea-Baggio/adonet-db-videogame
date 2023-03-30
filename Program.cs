efaf5f8b190d1147e77f2496ed13dfe9a9e58afd
Andrea-Baggio <andreabaggio800@gmail.com>
2023-03-30 16:35:06 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Data.SqlClient;
+
+
+var connStr = "Data Source = localhost;" +
+      "Initial Catalog = db_videogames; Integrated Security = True";
+
+var conn = new SqlConnection(connStr);
